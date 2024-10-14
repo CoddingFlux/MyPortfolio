4b499139b88a96abaf099dc68e918b42ea24a854
CoddingFlux <renish.l090@gmail.com>
2024-10-15 01:02:40 +0530

    Add variable.css file

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -3,12 +3,17 @@ let them=document.querySelector("#theam")
 let icon = document.getElementById("theam");
 const enableLight=()=>{
     document.body.classList.add("light_mode")
+    let img=document.querySelector('.aboutme')
+    img.style.backgroundImage="url('')"
+
     them.classList.remove("ri-sun-fill")
     them.classList.add(".mode","ri-moon-fill")
     localStorage.setItem('light','active')
 }
 const disableLight = () => {
+
     document.body.classList.remove("light_mode")
+    document.querySelector('.aboutme').style.backgroundImage="url('assets/image/img/About_me.png')"
     them.classList.add("ri-sun-fill")
     them.classList.remove(".mode","ri-moon-fill")
     localStorage.setItem('light',null)
