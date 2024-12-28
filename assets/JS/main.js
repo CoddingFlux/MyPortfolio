48430a9988caa29d454f273b4eca29ccf05c09aa
CoddingFlux <renish.l090@gmail.com>
2024-12-28 23:45:58 +0530

    changes in all files

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -3,8 +3,10 @@ let them=document.querySelector("#theam")
 let icon = document.getElementById("theam");
 const enableLight=()=>{
     document.body.classList.add("light_mode")
-    let img=document.querySelector('.aboutme')
-    img.style.backgroundImage="url('')"
+    let img1=document.querySelector('.aboutme')
+    let img2=document.querySelector('.skills')
+    img1.style.backgroundImage="url('')"
+    img2.style.backgroundImage="url('')"
 
     them.classList.remove("ri-sun-fill")
     them.classList.add(".mode","ri-moon-fill")
@@ -24,3 +26,14 @@ icon.onclick=()=>{
     light=localStorage.getItem('light')
     light !== "active" ? enableLight() : disableLight()
 }
+
+function openPopup(popupId) {
+    document.getElementById(popupId).style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
+  }
+
+  function closePopup() {
+    const popups = document.querySelectorAll('.popup');
+    popups.forEach(popup => popup.style.display = 'none');
+    document.getElementById('overlay').style.display = 'none';
+  }
